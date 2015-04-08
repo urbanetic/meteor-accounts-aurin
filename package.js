610a05ab7f178a1003eec3b4860d2a64280fe0a1
Aram Kocharyan <akarmenia@gmail.com>
2015-04-08 15:23:33 +1000

    Updated and implied accounts-ui. Version bump.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'urbanetic:accounts-aurin',
   summary: 'An accounts provider for AURIN workbench applications.',
   git: 'https://github.com/urbanetic/meteor-accounts-aurin.git',
-  version: '0.2.1'
+  version: '0.2.2'
 });
 
 Package.on_use(function(api) {
@@ -13,20 +13,21 @@ Package.on_use(function(api) {
     'http',
     'accounts-password',
     'accounts-ui',
-    'urbanetic:accounts-ui@0.1.4',
+    'urbanetic:accounts-ui@0.2.0',
     'aramk:utility@0.8.2',
     'matb33:collection-hooks@0.7.6'
   ], ['client', 'server']);
   api.use(['templating', 'jquery', 'less'], 'client');
   api.use(['iron:router@1.0.3'], 'client', {weak: true});
-  api.add_files(['src/common.coffee'], ['client', 'server']);
-  api.add_files(['src/server.coffee'], 'server');
-  api.add_files([
+  api.addFiles(['src/common.coffee'], ['client', 'server']);
+  api.addFiles(['src/server.coffee'], 'server');
+  api.addFiles([
     'src/client.coffee',
     'src/aurinLoginForm.html',
     'src/aurinLoginForm.less',
     'src/aurinLoginForm.coffee',
     'src/resources/aurin.jpg'
   ], 'client');
+  api.imply('urbanetic:accounts-ui');
   api.export('AccountsAurin', ['client', 'server']);
 });
